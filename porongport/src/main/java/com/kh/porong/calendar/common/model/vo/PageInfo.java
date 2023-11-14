d0a644011f48830988f377ed7fcb939ee7073eca
JIYOON <noje1123@naver.com>
2023-11-14 10:18:47 +0900

    common/pageInfo, pagination

---
+++ porongport/src/main/java/com/kh/porong/calendar/common/model/vo/PageInfo.java
@@ -0,0 +1,18 @@
+package com.kh.spring.common.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class PageInfo {
+	
+	private int listCount;
+	private int currentPage;
+	private int boardLimit;
+	private int pageLimit;
+	
+	private int maxPage;
+	private int startPage;
+	private int endPage;
+}
---
+++ porongport/src/main/java/com/kh/porong/calendar/common/template/Pagination.java
@@ -0,0 +1,18 @@
+package com.kh.spring.common.template;
+
+import com.kh.spring.common.model.vo.PageInfo;
+
+public class Pagination {
+
+	public static PageInfo getPageInfo(int listCount, int currentPage, int boardLimit, int pageLimit) {
+		
+		int maxPage = (int)Math.ceil((double)listCount/boardLimit);
+		int startPage = (currentPage-1)/pageLimit*pageLimit+1;
+		int endPage = startPage+pageLimit-1;
+		
+		if(endPage>maxPage) endPage = maxPage;
+		
+		return new PageInfo(listCount, currentPage, boardLimit, pageLimit, maxPage, startPage, endPage);
+	}
+	
+}
