b6499d3b468ea08d06aed6978e7eec5a3ce76c1f
Luvcong <9211087@gmail.com>
2023-11-30 12:04:37 +0900

    develop notice / noticeCount
    공지사항 게시글 조회수 기능 구현

--- porongport/src/main/java/com/kh/porong/notice/model/service/NoticeService.java
+++ porongport/src/main/java/com/kh/porong/notice/model/service/NoticeService.java
@@ -11,10 +11,14 @@ import com.kh.porong.reply.vo.Reply;
 
 public interface NoticeService {
 	
-	// 공지사항 게시글 전체 리스트 조회
+	// ==================================================================================
+	// 공지사항 게시판 - 리스트 조회 페이지 관련
+	// ==================================================================================
+	
+	// 1) 공지사항 게시글 전체 리스트 조회
 	ArrayList<Notice> noticeList(PageInfo pi);
 	
-	// 공지사항 게시글 전체 개수 조회
+	// 2) 공지사항 게시글 전체 개수 조회
 	int noticeListCount();
 	
 	// 공지사항 게시글 검색 리스트 조회
@@ -23,36 +27,42 @@ public interface NoticeService {
 	// 공지사항 게시글 검색 개수 조회
 	int searchNoticeCount(Map<String, Object> map);
 	
-	// 공지사항 게시글 작성
+	
+	// ==================================================================================
+	// 공지사항 게시판 - 작성 페이지 관련
+	// ==================================================================================
+	
+	// 1) 공지사항 게시글 작성
 	int insertNotice(Notice n);
 	
-	// 공지사항 게시글 작성 - 첨부파일
+	// 2) 공지사항 게시글 작성 - 첨부파일 추가
 	int insertAttachment(NoticeAttachment at);
 	
-	// 공지사항 게시글 삭제
-	int deleteNotice(int noticeNo);
 	
-	// 공지사항 게시글 상세보기
+	// ==================================================================================
+	// 공지사항 게시판 - 상세보기 페이지 관련
+	// ==================================================================================
+	
+	// 1) 공지사항 게시글 상세보기
 	List<Notice> detailNotice(Map<String, Object> map);
 	
-	// 공지사항 게시글 조회 수 증가
-	int increaseCount(int noticeNo);
-	
-	// 공지사항 게시글 좋아요 여부 체크
+	// 2) 공지사항 게시글 좋아요 여부 체크
 	int checkNoticeLike(Map<String, Object> map);
 	
-	// 공지사항 게시글 좋아요 수 증가
-	int likeCount(int noticeNo);
-	
-	// 공지사항 게시글 좋아요 취소
+	// 3) 공지사항 게시글 좋아요 취소
 	int deleteNoticeLike(Map<String, Object> map);
 	
-	// 공지사항 게시글 좋아요 추가
+	// 4) 공지사항 게시글 좋아요 추가
 	int insertNoticeLike(Map<String, Object> map);
 	
+	// 5) 공지사항 게시글 조회 수 증가
+	int increaseCount(int noticeNo);
 	
 	// 공지사항 게시글 수정
 	int updateNotice(int noticeNo);
+	
+	// 공지사항 게시글 삭제
+	int deleteNotice(int noticeNo);
 	
 	// 공지사항 게시글 댓글 리스트 조회
 	ArrayList<Reply> selectReplyList(int noticeNo);
